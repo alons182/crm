437ee9486e572b6e0ad753bd7606d648d4e33d29
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-10-21 16:27:46 -0600

    se agrego testnotification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Inspire::class,
-        Commands\tasksNotification::class
+        Commands\tasksNotification::class,
+        Commands\testNotification::class
     ];
 
     /**
