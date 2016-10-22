ac28d327d92a10c155ddc070adf637704c50abb7
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-10-22 00:05:04 -0600

    se quito comentario de tasknotification

--- app/Console/Commands/tasksNotification.php
+++ app/Console/Commands/tasksNotification.php
@@ -63,11 +63,7 @@ class tasksNotification extends Command
                     
                     try {
                         $this->mailer->notificationTasks(['task'=> $task]);
-                        /* Mail::send('emails.notifications.notificationTasks', ['task' => $task], function ($m) use ($task) {
-                            $m->from('info@vivendacr.com', 'CRM Vivenda');
-
-                            $m->to($task->notification_to, 'vivenda')->subject('Recordatorio de tarea!');
-                        });*/
+                        
                     }catch (Swift_RfcComplianceException $e)
                     {
                         Log::error($e->getMessage());
