a4b1fa921d554c0de6f90c9703d83466a3a1f33c
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-10-21 23:09:44 -0600

    se quito execute route

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,8 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Inspire::class,
-        Commands\tasksNotification::class,
-        Commands\executeRoute::class
+        Commands\tasksNotification::class
+
 
     ];
 
