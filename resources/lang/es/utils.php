eeaf321c801d223a81c4b38085489252e1886d80
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-11-13 16:40:18 -0600

    se cambio el locade es y se agrego lang es

---
+++ resources/lang/es/utils.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'status_client'  => [
+        '0' => '',
+        '1' => 'Finalizado',
+        '2' => 'Pre-Aprobado',
+        '3' => 'Interesado',
+        '4' => "Denegado",
+    ],
+    'status_color'  => [
+        '0' => 'default',
+        '1' => 'info',
+        '2' => 'success',
+        '3' => 'warning',
+        '4' => "danger",
+    ],
+    'debts_client'  => [
+        '0' => '',
+        '1' => 'No deudas',
+        '2' => 'Si deudas',
+        '3' => 'Por consultar',
+        '4' => "Monto especifico",
+    ],
+    'potencial_client'  => [
+        '0' => '',
+        '1' => 'Alto',
+        '2' => 'Medio',
+        '3' => 'Bajo',
+        
+    ],
+    'export_fields'  => [
+        'ide'=> 'Ide',
+        'fullname'=> 'Nombre completo',
+        'company'=> 'Compañia',
+        'job'=> 'Trabajo',
+        'email'=> 'Email',
+        'web'=> 'Web',
+        'phone1'=> 'Teléfono 1',
+        'phone2'=> 'Teléfono 1',
+        'comments'=> 'Comentarios',
+        'address'=> 'Dirección',
+        'referred'=> 'Referido',
+        'referred_others'=> 'Referido Otros',
+        'image'=> 'Imagen',
+        'status'=> 'Estatus',
+        'income'=> 'Ingresos',
+        'debts'=> 'Deudas',
+        'debts_amount'=> 'Monto específico deudas',
+        'potencial'=> 'Potencial',
+        'prima' => 'Prima',
+        
+    ],
+    
+
+];
