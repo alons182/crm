217a4f0f00498613d3699edfb3576c85343aeb48
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-02-07 12:32:13 -0600

    fix datepicker en create property

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.template')
 
+<link rel="stylesheet" href="/vendor/classic.css">
+<link rel="stylesheet" href="/vendor/classic.date.css">
 
 @section('content')
 
@@ -16,10 +18,25 @@
 @endsection
 @section('scripts')
 <script src="/vendor/jquery.maskedinput.min.js"></script>
-	<script type="text/javascript">
+<script src="/vendor/picker.js"></script>
+<script src="/vendor/picker.date.js"></script>
+<script type="text/javascript">
 		
 		$("#owner_phone1").mask("9999-9999");
 		$("#owner_phone2").mask("9999-9999");
+
+		$('.datepicker').pickadate({
+        monthsFull: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+        monthsShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+        weekdaysFull: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+        weekdaysShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+        today: 'hoy',
+        clear: 'borrar',
+        close: 'cerrar',
+        firstDay: 1,
+        format: 'yyyy-mm-dd',
+        formatSubmit: 'yyyy-mm-dd'
+    });
 		
-	</script>
+</script>
 @stop
